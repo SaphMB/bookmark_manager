b12a89c90616daa8f9731dbf5bcf55e975b943a6
Toby Hamand <tobyhamand@gmail.com>
2017-07-25 16:40:11 +0100

    refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,16 @@ Capybara.app = Bookmarks
 
 RSpec.configure do |config|
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
