db8f20e509a2c9a1d923367f8d22e9e0aa9a9aff
Alison Woodman <aliwoodman@gmail.com>
2017-07-26 15:35:38 +0100

    testing

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,8 +4,8 @@ require 'dm-postgres-adapter'
 require_relative 'models/link'
 
 #
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmark_manager_development')
-# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"))
+#DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmark_manager_development')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"))
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
