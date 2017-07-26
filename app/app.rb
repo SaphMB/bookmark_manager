bac1f782bdf2ce0bb624b190642e55a791648057
artemis <artemisxen@gmail.com>
2017-07-26 15:04:05 +0100

    testing

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,8 @@
-# ENV["RACK_ENV"] = "development"
+ENV["RACK_ENV"] = "development"
 
 require 'sinatra/base'
 require_relative './models/link.rb'
+require_relative 'data_mapper_setup.rb'
 
 class Bookmark < Sinatra::Base
 
